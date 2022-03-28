50d18988ccea0b84e362b0d68ffd7ad6173ddd23
GeekmasterK <ktheiss1990@gmail.com>
2022-03-28 10:21:35 -0700

    Tested trigger detection with debug statements.

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -8,4 +8,9 @@ public class Collision : MonoBehaviour
     {
         Debug.Log("CRASH!");
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Ran over something!");
+    }
 }
