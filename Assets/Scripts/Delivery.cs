f2faf5ff85821d56aa186b9081b607ca7cb8c1f4
GeekmasterK <ktheiss1990@gmail.com>
2022-03-29 12:13:34 -0700

    Added logic to change the player's color when they pick up a package, and back when they deliver the package.

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -7,6 +7,16 @@ public class Delivery : MonoBehaviour
     bool hasPackage = false;
     [SerializeField] float destroyDelay = 0.5f;
 
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();    
+    }
+
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("CRASH!");
@@ -19,12 +29,14 @@ public class Delivery : MonoBehaviour
             Debug.Log("Picked up the package!");
             hasPackage = true;
             Destroy(other.gameObject, destroyDelay);
+            spriteRenderer.color = hasPackageColor;
         }
 
         if(other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package delivered!");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
