49240228bcdc08ac88919fdf5a7b64797bdc3ad7
Zodd1990 <ktheiss1990@gmail.com>
2022-04-21 14:05:46 -0700

    Added a speed boost system and slow down when bumping into objects.

--- Assets/Scripts/Drive.cs
+++ Assets/Scripts/Drive.cs
@@ -6,12 +6,8 @@ public class Drive : MonoBehaviour
 {
     [SerializeField] float turnSpeed = 100f;
     [SerializeField] float moveSpeed = 15f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float slowSpeed = 10f;
+    [SerializeField] float boostSpeed = 25f;
 
     // Update is called once per frame
     void Update()
@@ -20,5 +16,18 @@ public class Drive : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0f, 0f, -turnAmount);
         transform.Translate(0f, moveAmount, 0f);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "SpeedUp")
+        {
+            moveSpeed = boostSpeed;
+        }
+    }
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;        
     }
 }
